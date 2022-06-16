74d659b67486bb5bcdfedea172ed39751d464558
ian <iancmilan@unochapeco.edu.br>
2022-06-15 22:34:03 -0300

    web

---
+++ web/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        // brand é o nome que damos para a cor, 500 é o tom
+        brand: {
+          500: '#8257e6',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
